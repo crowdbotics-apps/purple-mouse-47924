622c51a0f8d30a790683db3e98748660c7a4a418
Crowdbotics <team@crowdbotics.com>
2024-05-14 17:46:53 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "purple-mouse-47924",
+  url: "https://purple-mouse-47924.botics.co",
+  api: "https://purple-mouse-47924.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
